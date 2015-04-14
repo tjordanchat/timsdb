b518b004601c448b788999c78b19b3788ebce4cb
Tim Jordan <jjordan@jetblue.com>
2015-04-14 10:37:56 -0400

    gitlab hook

---
+++ bin/server.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'webrick'
+require 'json'
+require 'yaml'
+
+server = WEBrick::HTTPServer.new(:Port => 6060)
+server.mount_proc '/' do |req, res|
+  puts req.body
+  p = JSON.parse(req.body)
+  if p["ref"] == "refs/heads/develop"
+    remote = p["repository"]["url"]
+    email = p["commits"][0]["author"]["email"]
+    url = p["repository"]["url"]
+    after = p["after"]
+    pat = /[^:]*:(.*)\.git/
+    job = pat.match(url)[1]
+    command="git archive --remote=ssh://%s develop build.yml | tar xvf -" % remote.gsub(":","/")
+    puts `#{command}`
+    cfg = YAML.load_file('build.yml')
+    if cfg.length > 0
+      env = cfg['env']
+      command = "java -jar /home/JJordan/jenkins-cli.jar -i /home/JJordan/.ssh/id_rsa build %s -p env=%s -p url=%s -p email=%s -p commit=%s" % [job,env, url,email,after]
+      ENV['JENKINS_URL']="http://10.156.74.30:8080"
+      puts command
+      puts `#{command}`
+    end
+  end
+end
+
+trap 'INT' do 
+  server.shutdown 
+end
+server.start
